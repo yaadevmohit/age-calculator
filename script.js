912eed56c9e5d9b30c039d5539d735b44d2b1e95
yaadevmohit <mohityadav530@gmail.com>
2023-11-14 11:03:37 -0500

    add months logic

--- script.js
+++ script.js
@@ -8,12 +8,14 @@ const yearInput = document.getElementById("year-input")
 const submitButton = document.getElementById("submit")
 
 const currentDate = new Date()
+
+
 submitButton.addEventListener("click", (e) => {
     e.preventDefault()
     const birthDate = new Date(yearInput.value, monthInput.value, dayInput.value)
     const age = (currentDate - birthDate) / (1000 * 60 * 60 * 24)
     years.textContent = Math.floor(parseInt(age) / 365)
-    months.textContent = (parseInt(age) % 365)
-    
+    months.textContent = Math.round((parseInt(age) % 365) / 30)
+    days.textContent = (parseInt(age) % 365) % 30
 })
 
